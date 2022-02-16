39584fdac7aa49aad7dd8c50b264edfe7207d624
LuizGustavo <gustavol2010@yahoo.com>
2022-02-16 14:07:06 -0300

    estydo

--- cliente-generico.js
+++ cliente-generico.js
@@ -1,13 +1,13 @@
 //ABAIXO TEMOS UMA FUNÇÃO CONSTRUTORA
 
 function cliente(nome, cpf, email, saldo) {
-     this.nome = nome;
-     this.cpf = cpf;
-     this.email = email;
-     this.saldo = saldo;
-     this.depositar = function(valor) {
-         this.saldo += valor;
-     }
+    this.nome = nome;
+    this.cpf = cpf;
+    this.email = email;
+    this.saldo = saldo;
+    this.depositar = function (valor) {
+        this.saldo += valor;
+    }
 }
 
 
@@ -23,24 +23,25 @@ const luiz = new cliente("Luiz", "000 000 000 00", "gustavol2010@yahoo.com", 150
 
 
 
-function CadastroMotorista (nome, cpf, cnh, veiculo, revavam, vencimentoCarteira){
+function CadastroMotorista(id,nome, cpf, cnh, veiculo, revavam, vencimentoCarteira) {
+    this.id = id;
     this.nome = nome;
     this.cpf = cpf;
     this.cnh = cnh;
     this.veiculo = veiculo;
     this.revavam = revavam;
-    this.vencimentoCarteira=vencimentoCarteira;
+    this.vencimentoCarteira = vencimentoCarteira;
 }
 
-const Motorista1 = new CadastroMotorista (" Marcos ", " 000 000 000 00", "B" , "Caminhão 4 eixos", " 234765456" , 15/10/2022)
-const Motorista2 = new CadastroMotorista ("Luiz Carlos" , "111 111 111 11" , " D" , "Carreta" , " 777777777" , 01/02/2023)
-const Motorista3 = new CadastroMotorista ( "Juliano" , "222 222 222 22" , "D" ,  "Kombi" , "333333333" , 20/04/2023)
-const Motorista4 = new CadastroMotorista ( "Guilherme" , "333 333 333 33" , "444 444 444 44" , 25/05/2025 )
+const Motorista1 = new CadastroMotorista("1"," Marcos ", " 000 000 000 00", "B", "Caminhão 4 eixos", " 234765456", "15 / 10 / 2022")
+const Motorista2 = new CadastroMotorista("2","Luiz Carlos", "111 111 111 11", " D", "Carreta", " 777777777", "01 / 02 / 2023")
+const Motorista3 = new CadastroMotorista("3","Juliano", "222 222 222 22", "D", "Kombi", "333333333", "20 / 04 / 2023")
+const Motorista4 = new CadastroMotorista("4","Guilherme", "333 333 333 33", "B", "Scanner", "444 444 444 44", "25 / 05 / 2025")
 
 console.log(Motorista1)
-console.log(Motorista2) 
+console.log(Motorista2)
 console.log(Motorista3)
-console.log(Motorista3)
+console.log(Motorista4)
 
 
 
@@ -62,7 +63,7 @@ console.log(Motorista3)
 
 
 
-function abrirConta(nome, cpf, cartao, id, TipoDeConta, DepositarDinheiro){
+function abrirConta(nome, cpf, cartao, id, TipoDeConta, DepositarDinheiro) {
 
     this.nome = nome;
     this.cpf = cpf;
@@ -73,6 +74,6 @@ function abrirConta(nome, cpf, cartao, id, TipoDeConta, DepositarDinheiro){
 
 }
 
-const cliente1 = new abrirConta("Pedro" , "111222333 00" , "Santander" , "Poupança" , 10000)
+const cliente1 = new abrirConta("Pedro", "111222333 00", "Santander", "Poupança", 10000)
 
 //console.log(cliente1)
